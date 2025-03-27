467086e039e0a1eacc55d2da30b8676af3abf956
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-03-27 14:35:47 +0500

    deploy qilinmoqda

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import logo from './assets/logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+
+      </header>
+
+
+      <main>
+
+      </main>
+
+
+      <footer>
+        
+      </footer>
+
+      <div className=''>
+        <p>Test</p>
+        <img className='w-[200px]' src={logo} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
